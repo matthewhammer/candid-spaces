a8d5f7560ec9d9e07687449d447d7f3653d777d2
Matthew Hammer <matthew.hammer@gmail.com>
2021-05-26 08:48:33 -0600

    rename

--- test/service/accessControl.test.sh
+++ test/service/accessControl.test.sh
@@ -1,20 +1,21 @@
 #!ic-repl -r http://localhost:8000
 
-import CanCan = "rwlgt-iiaaa-aaaaa-aaaaa-cai";
+// service address
+import S = "rwlgt-iiaaa-aaaaa-aaaaa-cai";
 
 identity Alice;
 
-call CanCan.createProfile("alice", null);
+call S.createProfile("alice", null);
 assert _ != (null : opt null);
 
 identity Bob;
 
-call CanCan.getProfileInfo("alice", null); // fail: no profile for bob yet.
+call S.getProfileInfo("alice", null); // fail: no profile for bob yet.
 assert _ == (null : opt null);
 
-call CanCan.createProfile("bob", null); // ok.
+call S.createProfile("bob", null); // ok.
 assert _ != (null : opt null);
 
-call CanCan.getProfileInfo("alice", null); // ok
+call S.getProfileInfo("alice", null); // ok
 assert _ != (null : opt null);
 
