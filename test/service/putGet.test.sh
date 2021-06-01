933a87caa95f588ec34adbaf0a4f414058c31774
Matthew Hammer <matthew.hammer@gmail.com>
2021-06-01 16:00:29 -0600

    simpler data for test

--- test/service/putGet.test.sh
+++ test/service/putGet.test.sh
@@ -18,16 +18,26 @@ call S.put("alice", vec { "fib" }, vec { variant { nat = 5 } ; variant { nat = 8
 assert _ != (null : opt null);
 
 
-call S.put("alice", vec { "colorVariants" }, vec {
-            variant { variant = { name = "red" ; value = variant { nil } } };
-            variant { variant = { name = "green" ; value = variant { nil } } };
-            variant { variant = { name = "gold" ; value = variant { nil } } };            
-           });
+call S.put("alice", vec { "factorial" }, vec { variant { nat = 1 } ; variant { nat = 1 } });
+assert _ != (null : opt null);
+
+call S.put("alice", vec { "factorial" }, vec { variant { nat = 2 } ; variant { nat = 6 } });
+assert _ != (null : opt null);
+
+call S.put("alice", vec { "factorial" }, vec { variant { nat = 24 } ; variant { nat = 120 } });
 assert _ != (null : opt null);
 
 
+// call S.put("alice", vec { "colorVariants" }, vec {
+//            variant { variant = { name = "red" ; value = variant { nil } } };
+//            variant { variant = { name = "green" ; value = variant { nil } } };
+//            variant { variant = { name = "gold" ; value = variant { nil } } };
+//           });
+// assert _ != (null : opt null);
+
+
 let viewId = call S.createView("alice", vec { variant { space = vec { "fib" } };
-                                              variant { space = vec { "colorVariants" } } });
+                                              variant { space = vec { "factorial" } } });
 assert _ != (null : opt null);
 
 let fullImage = call S.getFullImage("alice", viewId);
